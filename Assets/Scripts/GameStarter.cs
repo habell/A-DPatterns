b866e6c0d1115e5b3fb28dc51ba90365dfadac3a
Habel <tamikys15@gmail.com>
2022-09-10 16:37:35 +0300

    monster pool spawn

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -8,8 +8,13 @@ namespace Asteroids
         {
             EnemyPool enemyPool = new EnemyPool(5); 
             var enemy = enemyPool.GetEnemy("Asteroid");
-            enemy.transform.position = Vector3.one;
-            enemy.gameObject.SetActive(true);
+            
+            EnemyPool enemyPool1 = new EnemyPool(5); 
+            var enemy1 = enemyPool1.GetEnemy("Monster");
+            //enemy.transform.position = Vector3.one;
+            //enemy.gameObject.SetActive(true);
+            enemy.ActiveEnemy(Vector3.one, Quaternion.identity);
+            enemy1.ActiveEnemy(Vector3.one, Quaternion.identity);
         }
     }
 }
