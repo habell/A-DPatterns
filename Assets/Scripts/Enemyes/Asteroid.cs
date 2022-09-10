5c3530e6b1c18b15eec81501a68266e572fd79af
Habel <tamikys15@gmail.com>
2022-09-10 16:32:18 +0300

    add death and refresh func

--- Assets/Scripts/Enemyes/Asteroid.cs
+++ Assets/Scripts/Enemyes/Asteroid.cs
@@ -1,25 +1,34 @@
 using System;
 using Enums;
+using JetBrains.Annotations;
 using ScriptableObjects;
 using Unity.Mathematics;
 using UnityEngine;
 
 namespace Asteroids
 {
-    public sealed class Asteroid : Enemy, IEnemy
+    public sealed class Asteroid : Enemy, IHealth
     {
         private Rigidbody _rigidbody;
+        
+        public Health Health { get; set; }
+
+        public void Death()
+        {
+            ReturnToPool();
+        }
 
         private void Awake()
         {
             _rigidbody = gameObject.AddComponent<Rigidbody>();
             _rigidbody.useGravity = false;
             _rigidbody.freezeRotation = true;
+            RefreshParameters();
         }
 
         private void Update()
         {
-            _rigidbody.AddForce(Vector3.up / 100);
+            _rigidbody.AddForce(Vector3.up / (100 - Preset.EnemyStruct.Speed));
         }
 
         private void OnCollisionEnter(Collision collision)
@@ -27,7 +36,14 @@ namespace Asteroids
             if (collision.gameObject.TryGetComponent(out Player ply))
             {
                 ply.Health.ChangeCurrentHealth(5);
+                Death();
             }
+        }
+
+        protected override void RefreshParameters()
+        {
+            var hp = Preset.EnemyStruct.Health;
+            Health = new Health(hp, hp);
         }
     }
 }
